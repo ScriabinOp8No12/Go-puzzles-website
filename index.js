71cd8c699e11f9b83522f2d5aa4853fe68eda3f2
nharwit <nharwit@gmail.com>
2023-05-25 23:55:20 -0600

    successfully added logo and go board puzzle image, color palette looks decent for dark theme

---
+++ index.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+  // properly creates a hover effect, but CSS is better for this!
+  // const homeButton = document.querySelector(".home");
+  // homeButton.addEventListener("mouseover", () => {
+  //   homeButton.style.backgroundColor = "green";
+  // });
+  // homeButton.addEventListener("mouseout", () => {
+  //   homeButton.style.backgroundColor = "";
+  // });
+});
