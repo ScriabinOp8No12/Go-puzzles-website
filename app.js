a82b23d1241fb5736ea1d9f62062d01c3a38feec
nharwit <nharwit@gmail.com>
2023-05-26 17:07:20 -0600

    got simple server working that takes a post request and stores the uploaded sgf into the uploads folder

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ app.post("/upload", upload.array("file", 10), (req, res) => {
   // req.files is an array of files uploaded
   // req.body will contain the text fields, if there were any
   console.log(req.files);
-  res.send("Files uploaded!");
+  res.send("File(s) uploaded!");
 });
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
