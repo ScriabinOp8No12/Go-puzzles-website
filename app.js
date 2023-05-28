1b33d6b73413309eab7dbaffce6822b19d71f0ed
nharwit <nharwit@gmail.com>
2023-05-28 10:19:04 -0600

    fixed up app.js server code, it still works, changed text to white color

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.post("/upload", upload.array("file", 10), (req, res) => {
   // req.files is an array of files uploaded
   // req.body will contain the text fields, if there were any
-  console.log(req.files);
+  console.log(req.files); // output is seen in the terminal
   res.send("File(s) uploaded!");
 });
 
--- pagesJS/sgf.js
+++ pagesJS/sgf.js
@@ -5,6 +5,7 @@ sgfForm.addEventListener("submit", (e) => {
   // .files accesses the files property of the selected input element,
   // which is a FileList object representing the files selected by the user
   const files = document.querySelector("#file").files;
+  // console.log(files.length);
   if (files.length > 10) {
     alert("You can only upload a maximum of 10 SGFs at once!");
     e.preventDefault();
