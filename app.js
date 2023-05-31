482ced7fcad4a142f4142396f1242ac3ce0de9d0
nharwit <nharwit@gmail.com>
2023-05-31 16:14:25 -0600

    copied some fetch function from chatGPT that kind of displays a json of all the uploaded SGFs, changed the button to one button that uploads once the user decides on which SGFs to upload

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ const upload = multer({ dest: "uploads/" });
 
 const app = express();
 
+// simple in-memory database
+const sgfs = [];
+
 app.post("/upload", upload.array("file", 10), (req, res) => {
   // req.files is an array of files uploaded
   // req.body will contain the text fields, if there were any
   console.log(req.files); // output is seen in the terminal
-  res.send("File(s) uploaded!");
+
+  // add each uploaded file to the database
+  req.files.forEach((file) => {
+    sgfs.push({
+      originalname: file.originalname,
+      uploadDate: new Date(),
+    });
+  });
+
+  res.json({ message: "File(s) uploaded!", count: req.files.length });
+});
+
+app.get("/sgfs", (req, res) => {
+  // return a list of all uploaded SGFs
+  res.json(sgfs);
 });
 
 const port = 8000;
--- pagesJS/sgf.js
+++ pagesJS/sgf.js
@@ -1,5 +1,23 @@
-// prevents user from uploading more than 10 SGFs at once!
+// uploads SGFs when user clicks open in their file explorer after selecting the SGF files
+document.getElementById("file").addEventListener("change", function () {
+  const form = document.getElementById("upload-sgf-form");
+  const formData = new FormData(form);
+  fetch(form.action, {
+    method: form.method,
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // update the page with the confirmation message and the table of uploaded SGFs
+      console.log(data.message); // File(s) uploaded!
+      console.log(data.count); // number of uploaded files
 
+      // fetch and display the updated list of uploaded SGFs
+      fetchAndDisplaySGFs();
+    });
+});
+
+// prevents user from uploading more than 10 SGFs at once!
 const sgfForm = document.querySelector("#upload-sgf-form");
 sgfForm.addEventListener("submit", (e) => {
   // .files accesses the files property of the selected input element,
@@ -11,3 +29,37 @@ sgfForm.addEventListener("submit", (e) => {
     e.preventDefault();
   }
 });
+
+function fetchAndDisplaySGFs() {
+  fetch("/sgfs")
+    .then((response) => response.json())
+    .then((sgfs) => {
+      // create a table to display the uploaded SGFs
+      const table = document.createElement("table");
+      const headerRow = document.createElement("tr");
+      const nameHeader = document.createElement("th");
+      nameHeader.textContent = "Name";
+      headerRow.appendChild(nameHeader);
+      const dateHeader = document.createElement("th");
+      dateHeader.textContent = "Upload Date";
+      headerRow.appendChild(dateHeader);
+      table.appendChild(headerRow);
+
+      sgfs.forEach((sgf) => {
+        const row = document.createElement("tr");
+        const nameCell = document.createElement("td");
+        nameCell.textContent = sgf.originalname;
+        row.appendChild(nameCell);
+        const dateCell = document.createElement("td");
+        dateCell.textContent = sgf.uploadDate;
+        row.appendChild(dateCell);
+        table.appendChild(row);
+      });
+
+      // append the table to the page
+      document.body.appendChild(table);
+    });
+}
+
+// call the function when the page loads
+fetchAndDisplaySGFs();
