552ea227073c358e3d4bd64ad176012f2f79052b
nharwit <nharwit@gmail.com>
2023-06-08 13:18:43 -0600

    properly created table, need to modify chatgpt code to append a new row to the table, AND not refresh the page when we click the submit button

--- pagesJS/sgf.js
+++ pagesJS/sgf.js
@@ -11,18 +11,31 @@ sgfForm.addEventListener("submit", (e) => {
     alert("You can only upload a maximum of 10 SGFs at once!");
     e.preventDefault();
   }
+  // new logic for adding the form data to the table
+  const table = document.querySelector("#sgf_table"); // replace '#my-table' with the selector for your table element
+
+  // get the form data
+  const formData = new FormData(sgfForm);
+  const gameID = formData.get("game-id"); // replace 'game-id' with the name of your game ID field
+  const dateUploaded = formData.get("date-uploaded"); // replace 'date-uploaded' with the name of your date uploaded field
+  const sgfName = formData.get("sgf-name"); // replace 'sgf-name' with the name of your SGF name field
+
+  // create a new row for the table
+  const newRow = document.createElement("tr");
+
+  // create cells for the new row and add the form data
+  const gameIDCell = document.createElement("td");
+  gameIDCell.textContent = gameID;
+  newRow.appendChild(gameIDCell);
+
+  const dateUploadedCell = document.createElement("td");
+  dateUploadedCell.textContent = dateUploaded;
+  newRow.appendChild(dateUploadedCell);
+
+  const sgfNameCell = document.createElement("td");
+  sgfNameCell.textContent = sgfName;
+  newRow.appendChild(sgfNameCell);
+
+  // add the new row to the table
+  table.appendChild(newRow);
 });
-
-// This code block below doesn't need to be here for everything to work...
-
-// // uploads SGFs when user clicks open in their file explorer after selecting the SGF files
-// document
-//   .getElementById("submit-button")
-//   .addEventListener("click", function (event) {
-//     event.preventDefault();
-//     document.getElementById("file").click();
-//   });
-
-// fileInput.addEventListener("change", () => {
-//   sgfForm.submit();
-// });
