74e3cc0d7f0cfb21da2e1f13cb14314177db3df3
nharwit <nharwit@gmail.com>
2023-07-03 13:17:03 -0600

    parse name, rank, result properly using regex! also properly tested with random sample of 16 SGFs

--- backend/parse_sgf.py
+++ backend/parse_sgf.py
@@ -4,30 +4,43 @@ import re
 # Set the directory containing the SGF files
 sgf_dir = './backend/uploads'
 
-# Get a list of all the SGF files in the directory
-sgf_files = [f for f in os.listdir(sgf_dir)]
+# Get a list of all the SGF (now .txt) files in the directory
+text_files = [f for f in os.listdir(sgf_dir)]
 
 # Process each SGF file
-for sgf_file in sgf_files:
-    # Construct the full path to the SGF file
-    sgf_path = os.path.join(sgf_dir, sgf_file)
+for text_file in text_files:
+    # Construct the full path to the text file
+    text_path = os.path.join(sgf_dir, text_file)
 
     # Open the SGF file and read its contents
-    with open(sgf_path, 'r') as f:
-        sgf_data = f.read()
+    with open(text_path, 'r') as f:
+        text_data = f.read()
 
-    # Process the SGF data below!
-    # ----------------------------------
+    # Process the text data below!
 
-    # Use regular expressions to search for the PB and PW properties (Player Black and Player White)
-    black_match = re.search(r'PB\[([^\]]+)\]', sgf_data)
-    white_match = re.search(r'PW\[([^\]]+)\]', sgf_data)
+    # Regex for PB and PW properties (Player Black and Player White)
+    black_player_match = re.search(r'PB\[([^\]]*)\]', text_data)
+    white_player_match = re.search(r'PW\[([^\]]*)\]', text_data)
 
-    # Extract the black and white player's names
-    black_player = black_match.group(1)
-    white_player = white_match.group(1)
+    # Extract Black and White player names
+    black_player = black_player_match.group(1) if black_player_match else None
+    white_player = white_player_match.group(1) if white_player_match else None
 
-    # Breaks on any special characters, almost nothing gets printed out...
-    # it was fixed by converting the .sgf file to .txt instead!
-    print("black: ", black_player)
-    print("white: ", white_player)
+    # Regex for BR and WR properties (Black Rank and White Rank)
+    rank_match = re.search(r'BR\[(.*?)\].*WR\[(.*?)\]', text_data, re.DOTALL)
+
+    # Extract the black and white player's ranks
+    black_rank = rank_match.group(1) if rank_match else "?"
+    white_rank = rank_match.group(2) if rank_match else "?"
+
+    # Regex for RE property (Result)
+    result_match = re.search(r'RE\[([^\]]*)\]', text_data)
+
+    # Extract the game result
+    result = result_match.group(1) if result_match else "?"
+
+    # Print the extracted information
+    print("Black Player:", black_player, "------- ",
+          "Black rank:", black_rank, "Result:", result)
+    print("White Player:", white_player, "------- ",
+          "White rank:", white_rank, "Result:", result)
