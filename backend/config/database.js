dce96e198705c50b41deee61106323f3564da4f9
nharwit <nharwit@gmail.com>
2023-07-05 17:18:59 -0600

    changed config/database.js to be same as aa open

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,23 +1,11 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+module.exports = {
+  development: {
+    storage: process.env.DB_FILE,
+    dialect: "sqlite",
+    seederStorage: "sequelize",
+    benchmark: true,
+    logQueryParameters: true,
+    typeValidation: true,
+    // logging: false
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+};
