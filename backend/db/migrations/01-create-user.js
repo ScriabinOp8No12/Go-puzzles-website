327fbfd251bd4b5b3b08428c636a614e6a52f817
nharwit <nharwit@gmail.com>
2023-07-13 14:53:22 -0600

    added model and migration for user with appropriate constraints and validations, working on seeder now

--- backend/db/migrations/01-create-user.js
+++ backend/db/migrations/01-create-user.js
@@ -1,40 +1,61 @@
-'use strict';
-/** @type {import('sequelize-cli').Migration} */
+"use strict";
+
+// let options = {};
+// if (process.env.NODE_ENV === "production") {
+//   options.schema = process.env.SCHEMA; // define your schema in options object
+// }
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      username: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      rank: {
-        type: Sequelize.STRING
-      },
-      solved_puzzles: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+    await queryInterface.createTable(
+      "Users",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        username: {
+          allowNull: false,
+          // max username characters 30
+          type: Sequelize.STRING(30),
+        },
+        email: {
+          allowNull: false,
+          unique: true,
+          // max email characters 256
+          type: Sequelize.STRING(256),
+        },
+        hashedPassword: {
+          allowNull: false,
+          type: Sequelize.STRING.BINARY,
+        },
+        rank: {
+          // ALLOW NULL because won't have initial data to know what rank the puzzle is
+          type: Sequelize.INTEGER,
+        },
+        solved_puzzles: {
+          allowNull: false,
+          defaultValue: 0,
+          type: Sequelize.INTEGER,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       }
-    });
+      // options
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
-  }
+    await queryInterface.dropTable("Users");
+    // await queryInterface.dropTable("Users", options);
+  },
 };
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model, Validator } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -13,15 +11,71 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    email: DataTypes.STRING,
-    rank: DataTypes.STRING,
-    solved_puzzles: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            // use Validator.isEmail (Validator package contains isEmail())
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be an email.");
+            }
+          },
+          // empty string model validation
+          notEmptyString(value) {
+            if (value.length === 0) {
+              throw new Error("Cannot be empty.");
+            }
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 256],
+          isEmail: true,
+        },
+      },
+      hashedPassword: {
+        allowNull: false,
+        // use .binary here
+        type: DataTypes.STRING.BINARY,
+        validate: {
+          len: [60, 60],
+        },
+      },
+      rank: {
+        type: DataTypes.INTEGER,
+        // using my own elo system for ranking puzzles, must not be negative, and maximum of 100,000 elo
+        validate: {
+          min: 0,
+          // realistically, something like a maximum of 5,000 elo should be plenty
+          max: 100000,
+        },
+      },
+      solved_puzzles: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        // solved number of puzzles must be between 0 and 1 trillion (can't be negative)
+        validate: {
+          min: 0,
+          max: 1000000000000,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"],
+        },
+      },
+    }
+  );
   return User;
 };
