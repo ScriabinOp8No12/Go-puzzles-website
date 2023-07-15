0460e953bfca98f346735424ef0554fedb885fff
nharwit <nharwit@gmail.com>
2023-07-14 22:30:39 -0600

    working with csrf and cors disabled, need to update backend/routes/api/users.js and session.js with different columns later

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,56 @@
 const express = require("express");
+require("express-async-errors");
+const morgan = require("morgan");
+// cors and csurf commented out!
+// const cors = require("cors");
+// const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+
+const { environment } = require("./config");
+const isProduction = environment === "production";
+// ***************************************
+// these 2 below are mine!
 const multer = require("multer");
 // IT GIVES PERMISSION DENIED if I changed dest below to "/backend/uploads/"
 // needs to be relative to the current folder, which is backend
 // ./uploads works great!
 const upload = multer({ dest: "./uploads" });
+// ***************************************
+// we are already getting all the routes from the folder, so no need to require individual routes
+const routes = require("./routes");
+const { ValidationError } = require("sequelize");
 
 const app = express();
+
+app.use(morgan("dev"));
+
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+// if (!isProduction) {
+//   // enable cors only in development
+//   app.use(cors());
+// }
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
+);
+
+// Set the _csrf token and create req.csrfToken method
+// app.use(
+//   csurf({
+//     cookie: {
+//       secure: isProduction,
+//       sameSite: isProduction && "Lax",
+//       httpOnly: true,
+//     },
+//   })
+// );
 
 // simple in-memory database
 const sgfs = [];
@@ -33,7 +78,43 @@ app.get("/sgfs", (req, res) => {
   res.json(sgfs);
 });
 
-const port = 8001;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.use(routes);
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
 });
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = "Validation error";
+    err.errors = errors;
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
+// const port = 8001;
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
+
+module.exports = app;
---
+++ backend/routes/api/index.js
@@ -0,0 +1,30 @@
+// backend/routes/api/index.js
+const router = require("express").Router();
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+// const spotsRouter = require("./spots.js"); // NEED TO ADD THESE HERE LATER ONCE WE CREATE THE ROUTERS!
+// const reviewsRouter = require("./reviews.js");
+// const reviewImagesRouter = require("./reviewImages.js");
+// const bookingsRouter = require("./bookings.js");
+// const spotImagesRouter = require("./spotImages.js");
+
+const { restoreUser } = require("../../utils/auth.js");
+
+// Connect restoreUser middleware to the API router
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
+router.use(restoreUser);
+
+router.use("/session", sessionRouter);
+router.use("/users", usersRouter);
+// router.use("/spots", spotsRouter);
+// router.use("/reviews", reviewsRouter);
+// router.use("/bookings", bookingsRouter);
+// router.use("/review-images", reviewImagesRouter);
+// router.use("/spot-images", spotImagesRouter);
+
+router.post("/test", (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+module.exports = router;
