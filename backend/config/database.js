44d30118365a6e96d3f694cb703eea137aa42684
nharwit <nharwit@gmail.com>
2023-07-14 20:14:49 -0600

    added psql-setup-script file, added index.js to backend/config, and modified database.js within config folder too

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,26 @@
+// backend/config/database.js
+const config = require("./index");
+
 module.exports = {
   development: {
-    storage: process.env.DB_FILE,
+    storage: config.dbFile,
     dialect: "sqlite",
     seederStorage: "sequelize",
-    benchmark: true,
     logQueryParameters: true,
     typeValidation: true,
-    // logging: false
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    seederStorage: "sequelize",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    define: {
+      schema: process.env.SCHEMA,
+    },
   },
 };
