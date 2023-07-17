c2ef7e8251079ee6624fc4c7a739706e8a3c4445
nharwit <nharwit@gmail.com>
2023-07-17 12:16:48 -0600

    routes folder seems to be setup successfully, no errors in terminal

---
+++ backend/routes/api/puzzles.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { requireAuth } = require("../../utils/auth");
+const { User, Sgf, UserPuzzle } = require("../../db/models");
+const { Sequelize } = require("sequelize");
+const { Op } = require("sequelize");
+
+const router = express.Router();
+
+//
+//
+
+module.exports = router;
