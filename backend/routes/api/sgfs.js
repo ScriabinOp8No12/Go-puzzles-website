b30abeb57f7c1de5af9477464b4d4811930bb3f8
nharwit <nharwit@gmail.com>
2023-07-18 23:10:53 -0600

    might have gotten validate sgf to work with smartgame library, using jssgf to parse the sgf now instead of sgfmill because sgfmill is a python only library, still unsure how to actually run the python script within javscript route to generate the thumbnail image

--- backend/routes/api/sgfs.js
+++ backend/routes/api/sgfs.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const smartgame = require("smartgame");
+// const sgf2img = require("../../sgf2img.py");
+const jssgf = require("jssgf");
 const { requireAuth } = require("../../utils/auth");
 const { User, Puzzle, Sgf } = require("../../db/models");
 const { Sequelize } = require("sequelize");
 const { Op } = require("sequelize");
 
 const router = express.Router();
-
 // Get all SGFs of the current user (format like go4go.net)
 router.get("/current", requireAuth, async (req, res) => {
   // when making a request to "get current user" on postman, and result is { "user": null}
@@ -74,19 +76,72 @@ router.post("/current", requireAuth, async (req, res) => {
       },
     });
   }
-  const sgfData = await Sgf.create({
-    game_preview,
-    id,
-    user_id,
-    createdAt,
-    updatedAt,
-    sgf_name,
-    black_player,
-    white_player,
-    black_rank,
-    white_rank,
-    result,
-  });
-  return res.json(sgfData);
+  try {
+    // Validate the sgf_data in the req.body
+    if (!Array.isArray(sgf_data)) {
+      return res.status(400).json({ error: "SGF data needs to be an array!" });
+    }
+
+    // Process each SGF in the array
+    const response = [];
+    for (const data of sgf_data) {
+      console.log("THIS IS THE DATA: ", data);
+      try {
+        // Parse the SGF data using smartgame
+        const collection = smartgame.parse(data);
+
+        // Check if the collection object is valid
+        if (
+          !collection ||
+          !collection.gameTrees ||
+          collection.gameTrees.length === 0
+        ) {
+          return res.status(400).json({ error: "Invalid SGF data" });
+        }
+      } catch (error) {
+        return res.status(400).json({ error: "Invalid SGF data" });
+      }
+
+      // Parse the SGF data using jssgf
+      const sgf = jssgf.parse(data);
+      const gameInfo = sgf[0].props;
+      const blackPlayer = gameInfo.PB[0];
+      const whitePlayer = gameInfo.PW[0];
+      const blackRank = gameInfo.BR[0];
+      const whiteRank = gameInfo.WR[0];
+      const result = gameInfo.RE[0];
+
+      // Generate the SGF name
+      const sgfName = `${blackPlayer} vs ${whitePlayer}`;
+
+      // Generate the preview image
+      // const previewImage = await sgf2img.generatePreview(data);
+
+      // Create a new Sgf record in the database
+      const sgfRecord = await Sgf.create({
+        sgf_data: data,
+        // game_preview: previewImage,
+        user_id: req.user.id,
+        sgf_name: sgfName,
+        black_player: blackPlayer,
+        white_player: whitePlayer,
+        black_rank: blackRank,
+        white_rank: whiteRank,
+        result: result,
+      });
+
+      // Add the new Sgf record to the response array
+      response.push(sgfRecord);
+    }
+
+    // Send a success response
+    res.status(201).json(response);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ error: "An error occurred while uploading the SGFs" });
+  }
 });
+
 module.exports = router;
