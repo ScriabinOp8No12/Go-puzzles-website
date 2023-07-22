d0e162b5f1c8cc1301e36c6ae8ba0d9b27180c7e
nharwit <nharwit@gmail.com>
2023-07-21 22:15:02 -0600

    still debugging render issue with sgfmill and pythonia

--- backend/routes/api/sgfs.js
+++ backend/routes/api/sgfs.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const smartgame = require("smartgame");
 const { python } = require("pythonia");
-console.log("Python version used by pythonia:", pythonia.pythonVersion);
 const path = require("path");
 const jssgf = require("jssgf");
 const { requireAuth } = require("../../utils/auth");
