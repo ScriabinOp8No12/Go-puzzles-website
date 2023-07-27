9c975de884990cc80f778978e6a5457d2d030cba
nharwit <nharwit@gmail.com>
2023-07-27 12:17:58 -0600

    created new migration and seeder for userpuzzle, added 3 columns

---
+++ backend/db/seeders/20230727161427-add-initial-seed-date-to-user-puzzles.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    // put tableName here inside up function instead of outside
+    options.tableName = "UserPuzzles";
+    return queryInterface.bulkInsert(
+      options,
+      [
+        {
+          user_id: 1,
+          puzzle_id: 4,
+          move_number: 50,
+          completed: true,
+          thumbnail: "base64 encoded string",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          user_id: 1,
+          puzzle_id: 5,
+          move_number: 50,
+          completed: false,
+          thumbnail: "base64 encoded string",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          user_id: 1,
+          puzzle_id: 6,
+          move_number: 50,
+          completed: true,
+          thumbnail: "base64 encoded string",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = "UserPuzzles";
+    return queryInterface.bulkDelete(options, null, {});
+  },
+};
