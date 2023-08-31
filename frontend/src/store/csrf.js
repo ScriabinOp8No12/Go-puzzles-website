d7b4a3a4ef01ea7c49fba9d575234f94d82d90b9
nharwit <nharwit@gmail.com>
2023-08-31 02:59:54 -0600

    added logo, modified index.html, added index.js to store, modified index.js in backend index.js, added csrf.js

---
+++ frontend/src/store/csrf.js
@@ -0,0 +1,38 @@
+import Cookies from "js-cookie";
+
+export async function csrfFetch(url, options = {}) {
+  // set options.method to 'GET' if there is no method
+  options.method = options.method || "GET";
+  // set options.headers to an empty object if there is no headers
+  options.headers = options.headers || {};
+
+  // if the options.method is not 'GET', then set the "Content-Type" header to
+  // "application/json", and set the "XSRF-TOKEN" header to the value of the
+  // "XSRF-TOKEN" cookie
+  // Updated for cloudinary!
+  if (options.method.toUpperCase() !== "GET") {
+    if (options.headers["Content-Type"] === "multipart/form-data") {
+      delete options.headers["Content-Type"];
+    } else {
+      options.headers["Content-Type"] =
+        options.headers["Content-Type"] || "application/json";
+    }
+    // Was XSRF-token originally in the .headers below
+    options.headers["XSRF-TOKEN"] = Cookies.get("XSRF-TOKEN");
+  }
+  // call the default window's fetch with the url and the options passed in
+  const res = await window.fetch(url, options);
+
+  // if the response status code is 400 or above, then throw an error with the
+  // error being the response
+  if (res.status >= 400) throw res;
+
+  // if the response status code is under 400, then return the response to the
+  // next promise chain
+  return res;
+}
+
+// call this to get the "XSRF-TOKEN" cookie, should only be used in development
+export function restoreCSRF() {
+  return csrfFetch("/api/csrf/restore");
+}
