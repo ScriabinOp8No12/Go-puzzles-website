5d4b9aa6ac50c8e1226b4c6499367a28d9b2da33
nharwit <nharwit@gmail.com>
2023-09-02 16:57:43 -0600

    got post sgfs component and thunk working to display a single sgf when we manually enter the sgf data in an useEffect, now we need to have the thumbnail and data show up after we click a button 'upload sgf'

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from "./session";
 import modalReducer from "./modal";
+import sgfReducer from "./sgfs";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   modal: modalReducer,
+  sgfs: sgfReducer,
 });
 
 let enhancer;
