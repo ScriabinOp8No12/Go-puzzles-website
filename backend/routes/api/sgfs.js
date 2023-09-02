5bd30a856da0139cd527e28a5c1470714653afd5
nharwit <nharwit@gmail.com>
2023-09-01 23:56:08 -0600

    working on styling home page, removed 2 px light grey border, trying to figure out best way to make the entire body black ish because it's getting overridden by the signup form background modal

--- backend/routes/api/sgfs.js
+++ backend/routes/api/sgfs.js
@@ -7,15 +7,15 @@ const jssgf = require("jssgf");
 const { requireAuth } = require("../../utils/auth");
 const { User, Puzzle, Sgf } = require("../../db/models");
 // doesn't work for somereason, javascript can't find cloudinary.js
-const cloudinary = require("../../../cloudinary.js");
+// const cloudinary = require("../../../cloudinary.js");
 
-// const cloudinary = require("cloudinary").v2;
+const cloudinary = require("cloudinary").v2;
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.CLOUD_API_KEY,
-//   api_secret: process.env.CLOUD_API_SECRET,
-// });
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 const router = express.Router();
 
