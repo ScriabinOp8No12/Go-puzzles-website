bcaa9e96f42808d106f6e4fd8b04f3c58b817889
nharwit <nharwit@gmail.com>
2023-09-05 14:20:51 -0600

    delete modal for sgfs working, but can't get specific errors showing up when inputting invalid sgf

---
+++ frontend/src/components/DeleteSgfModal.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import { deleteSgfByIdThunk } from "../store/sgfs";
+import "./styles/DeleteSgf.css"
+
+const DeleteSgfModal = ({ sgfId }) => {
+  const dispatch = useDispatch();
+  const modalRef = useRef();
+  const [showModal, setShowModal] = useState(false); // Initially set to false
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    dispatch(deleteSgfByIdThunk(sgfId))
+      .then(() => {
+        setShowModal(false); // Close the modal on successful deletion
+      })
+      .catch((error) => {
+        console.error("Failed to delete review:", error);
+      });
+  };
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setShowModal(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="deleteSgfButton">
+        <button className="trashcan-icon" onClick={() => setShowModal(true)}>🗑️</button>
+      </div>
+      {showModal && (
+        <div className={`modal-overlay ${showModal ? "activeOverlay" : ""}`}>
+          <div className="deleteModal" ref={modalRef}>
+            <form className="delete-modal-form">
+              <h1 className="deleteText">Confirm Delete</h1>
+              <p className="deleteConfirmation">Are you sure you want to delete this SGF?</p>
+              <button className="deleteButton" onClick={(e) => handleDelete(e)}>
+                Yes (Delete SGF)
+              </button>
+              <button
+                className="cancelButton"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setShowModal(false);
+                }}
+              >
+                No (Keep SGF)
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default DeleteSgfModal;
