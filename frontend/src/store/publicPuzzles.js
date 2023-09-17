6f651d64becdeefd4993072e152d7da37433b345
nharwit <nharwit@gmail.com>
2023-09-17 00:32:40 -0600

    edit puzzle done, lessons learned: don't forget await keyword, get requests can affect the state of the edit state because it needs to grab the values there, need to style now, that was over an hour of tough debugging

--- frontend/src/store/publicPuzzles.js
+++ frontend/src/store/publicPuzzles.js
@@ -1,94 +1,135 @@
-import { csrfFetch } from "./csrf"
+import { csrfFetch } from "./csrf";
 
 // ************* Action Type ***************** //
 
 // Get all public puzzles for landing page / home page
-const FETCH_PUBLIC_PUZZLES = "puzzles/FETCH_PUBLIC_PUZZLES"
-const FETCH_PUBLIC_PUZZLE_BY_ID = "puzzles/FETCH_PUBLIC_PUZZLE_BY_ID"
-const UPDATE_RANKINGS_AND_SOLVED_COUNTS = "puzzles/UPDATE_RANKINGS_AND_SOLVED_COUNTS"
+const FETCH_PUBLIC_PUZZLES = "puzzles/FETCH_PUBLIC_PUZZLES";
+const FETCH_PUBLIC_PUZZLE_BY_ID = "puzzles/FETCH_PUBLIC_PUZZLE_BY_ID";
+const UPDATE_RANKINGS_AND_SOLVED_COUNTS =
+  "puzzles/UPDATE_RANKINGS_AND_SOLVED_COUNTS";
+const EDIT_PUBLIC_PUZZLE_BY_ID = "/puzzles/EDIT_PUBLIC_PUZZLE_BY_ID";
 
 // ************* Action Creators ***************** //
 export const fetchPublicPuzzles = (puzzles) => ({
   type: FETCH_PUBLIC_PUZZLES,
-  payload: puzzles
-})
+  payload: puzzles,
+});
 
 export const fetchPublicPuzzleById = (puzzle) => ({
   type: FETCH_PUBLIC_PUZZLE_BY_ID,
-  payload: puzzle
-})
+  payload: puzzle,
+});
 
 export const updateRankingsAndSolvedCounter = (ranking) => ({
   type: UPDATE_RANKINGS_AND_SOLVED_COUNTS,
-  payload: ranking
-})
+  payload: ranking,
+});
+
+export const editPublicPuzzleById = (updatedPuzzle) => {
+  return {
+  type: EDIT_PUBLIC_PUZZLE_BY_ID,
+  payload: updatedPuzzle,
+  }
+};
+
 
 // ************* Thunks ***************** //
 
 export const fetchPublicPuzzlesThunk = () => async (dispatch) => {
-  const response = await csrfFetch("/api/puzzles")
+  const response = await csrfFetch("/api/puzzles");
 
   if (response.ok) {
-    const data = await response.json()
-    console.log("public puzzle thunk data: ", data)
-    dispatch(fetchPublicPuzzles(data.puzzles))
+    const data = await response.json();
+    // console.log("public puzzle thunk data: ", data);
+    dispatch(fetchPublicPuzzles(data.puzzles));
   }
-}
+};
 
 export const fetchPublicPuzzleByIdThunk = (puzzleId) => async (dispatch) => {
-  const response = await csrfFetch(`/api/puzzles/${puzzleId}`)
+  const response = await csrfFetch(`/api/puzzles/${puzzleId}`);
   // console.log("Inside fetchPublicPuzzleByIdThunk, *** puzzleId", puzzleId);
   if (response.ok) {
-    const data = await response.json()
-    dispatch(fetchPublicPuzzleById(data))
+    const data = await response.json();
+    dispatch(fetchPublicPuzzleById(data));
   }
-}
+};
 
-export const updateRankingsAndSolvedCounterThunk = (puzzleId, userWin) => async (dispatch) => {
-  const response = await csrfFetch(`/api/puzzles/${puzzleId}/ranking/update`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({userWin})
-  })
-  if (response.ok) {
-    const data = await response.json()
-    // console.log("Thunk data: ", data)
-    dispatch(updateRankingsAndSolvedCounter(data))
-  }
-}
+export const updateRankingsAndSolvedCounterThunk =
+  (puzzleId, userWin) => async (dispatch) => {
+    const response = await csrfFetch(
+      `/api/puzzles/${puzzleId}/ranking/update`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userWin }),
+      }
+    );
+    if (response.ok) {
+      const data = await response.json();
+      // console.log("Thunk data: ", data)
+      dispatch(updateRankingsAndSolvedCounter(data));
+    }
+  };
+
+export const editPublicPuzzlesByIdThunk =
+  (puzzleId, puzzleDetails) => async (dispatch) => {
+    const response = await csrfFetch(`/api/puzzles/${puzzleId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(puzzleDetails),
+    });
+    const updatedPuzzle = await response.json();
+    if (response.ok) {
+      dispatch(editPublicPuzzleById(updatedPuzzle));
+      return updatedPuzzle;
+    }
+  };
 
 // ************* Reducer ***************** //
 
 const initialState = {
   publicPuzzles: [],
   currentPublicPuzzle: null,
-  // Is this the correct initial state?
-  userWin: null
-}
+  userWin: null,
+};
 
 const publicPuzzlesReducer = (state = initialState, action) => {
-
-  switch(action.type) {
+  switch (action.type) {
     case FETCH_PUBLIC_PUZZLES:
       return {
         ...state,
-        publicPuzzles: action.payload
-      }
+        publicPuzzles: action.payload,
+      };
     case FETCH_PUBLIC_PUZZLE_BY_ID:
       return {
         ...state,
-        currentPublicPuzzle: action.payload
-      }
+        currentPublicPuzzle: action.payload,
+      };
     case UPDATE_RANKINGS_AND_SOLVED_COUNTS:
       return {
         ...state,
-        userWin: action.payload
-      }
-      default:
+        userWin: action.payload,
+      };
+    case EDIT_PUBLIC_PUZZLE_BY_ID:
+      return {
+        ...state,
+        publicPuzzles: state.publicPuzzles.map((puzzle) =>
+          puzzle.id === action.payload.id ? action.payload : puzzle
+        ),
+        // Change state of currentPublicPuzzle
+        currentPublicPuzzle:
+          state.currentPublicPuzzle &&
+          state.currentPublicPuzzle.id === action.payload.id
+            ? action.payload
+            : state.currentPublicPuzzle,
+      };
+    default:
       return state;
   }
-}
+};
 
-export default publicPuzzlesReducer
+export default publicPuzzlesReducer;
