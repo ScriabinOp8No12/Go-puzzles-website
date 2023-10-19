4ad48f9b5a20e7559da03e45e42ea90a38f0683c
nharwit <nharwit@gmail.com>
2023-10-19 12:18:52 -0600

    thumbnail of user puzzles properly displayed on user puzzles page now!

--- frontend/src/components/UserPuzzles.js
+++ frontend/src/components/UserPuzzles.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 // import { openModal } from "../store/modal";
 import { fetchAllUserPuzzlesThunk } from "../store/userPuzzles";
 import "./styles/UserPuzzles.css";
 
 const UserPuzzles = () => {
+  const history = useHistory()
   const dispatch = useDispatch()
   // state.slicename.property (state.reducer.publicPuzzles array)
   const userPuzzles = useSelector((state) => state.userPuzzles.userPuzzles);
+
+  console.log(userPuzzles)
 
   useEffect(() => {
     dispatch(fetchAllUserPuzzlesThunk())
@@ -16,7 +19,26 @@ const UserPuzzles = () => {
 
   return (
     <>
-    <h1>test</h1>
+    <div className="public-puzzle-table">
+        {userPuzzles &&
+          userPuzzles.map((userPuzzle, index) => (
+            <div className="public-puzzle-thumbnail" key={index}>
+              <img
+                src={userPuzzle.Puzzle.thumbnail}
+                alt={`Puzzle ${userPuzzle.Puzzle.id}`}
+                // Changed get /puzzles endpoint to id: puzzle.id instead of puzzle_id as the column name
+                onClick={() => history.push(`/user_puzzles/${userPuzzle.Puzzle.id}`)}
+                style={{ cursor: "pointer" }}
+              />
+              {/* <div className="puzzle-details">
+                  <div className = "puzzle-category">Category: {puzzle.category}</div>
+                  <div className = "puzzle-rank">Rank: {puzzle.difficulty}</div>
+                  <button className="public-puzzles-pencil-icon" onClick={() => openEditModal(puzzle.id)}>✏️</button>
+                  <SuspendPublicPuzzleModal puzzleId={puzzle.id} />
+              </div> */}
+            </div>
+          ))}
+      </div>
     </>
 
   )
