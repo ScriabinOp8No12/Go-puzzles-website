20dc4306aa935d5233034884923acf8dd52a7ecd
nharwit <nharwit@gmail.com>
2023-10-21 23:19:42 -0600

    got signup and login buttons to dynamically render only when the user isn't logged in, also clicking on them opens up the signup/login form modal, finished styling the main text and 3 h2s

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,16 +1,53 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import OpenModalMenuItem from "../components/Navigation/OpenModalMenuItem";
+import LoginFormModal from "./LoginFormModal";
+import SignupFormModal from "./SignupFormModal";
 import "./styles/LandingPage.css";
 
 const LandingPage = () => {
+  const user = useSelector((state) => state.session.user);
+
   return (
     <div className="go-container">
       {/* Left Column */}
-      <div className="home-page-left-column">
-        <h1 className="home-page-h1">Welcome to go-puzzles.com!</h1>
-      </div>
+      <div className="landing-page-left-column">
+        <div className="landing-page-header-section">
+          <h1 className="landing-page-h1">Welcome to go-puzzles.com!</h1>
+          <img
+            src="/turtle-logo.jpg"
+            alt="Landing Page Turtle Logo"
+            className="landing-page-turtle-logo-h1"
+          />
+        </div>
+        <h2 className="landing-page-slogan">
+          The best place on the web to level up your Go skills with full board
+          puzzles
+        </h2>
+        {!user && (
+          <>
+            <h2 className="landing-page-signup-login-button">
+              Please
+              <OpenModalMenuItem
+                itemText=" signup"
+                modalComponent={<SignupFormModal />}
+              />
+              or
+              <OpenModalMenuItem
+                itemText=" login"
+                modalComponent={<LoginFormModal />}
+              />
+              to use this website
+            </h2>
+          </>
+        )}
 
+        <h2>
+          Try puzzles at Public Puzzles, and upload games to generate puzzles
+        </h2>
+      </div>
       {/* Right Column */}
-      <div className="home-page-right-column"></div>
+      <div className="landing-page-right-column"></div>
     </div>
   );
 };
