c1017f0c02138508f546a4581f4ff233ffd8a62a
nharwit <nharwit@gmail.com>
2023-10-21 22:38:53 -0600

    rendered h1 on landing page, working on adding text and interactive buttons

---
+++ frontend/src/components/LandingPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles/LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="go-container">
+      {/* Left Column */}
+      <div className="home-page-left-column">
+        <h1 className="home-page-h1">Welcome to go-puzzles.com!</h1>
+      </div>
+
+      {/* Right Column */}
+      <div className="home-page-right-column"></div>
+    </div>
+  );
+};
+
+export default LandingPage;
