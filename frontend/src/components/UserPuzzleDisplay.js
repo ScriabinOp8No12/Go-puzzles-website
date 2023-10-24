1b50afdfbd2ce25b2f83067c32dc1d3adb571576
nharwit <nharwit@gmail.com>
2023-10-24 14:09:41 -0600

    userpuzzle display component seems to be working, need to make sure no other code broke, and that the glift go board isn't doing weird rendering stuff when we navigate between them

--- frontend/src/components/UserPuzzleDisplay.js
+++ frontend/src/components/UserPuzzleDisplay.js
@@ -10,68 +10,44 @@ import { fetchUserPuzzleByIdThunk } from "../store/userPuzzles";
 const UserPuzzleDisplay = () => {
   const { puzzle_id } = useParams();
   const dispatch = useDispatch();
-  // const [currentPuzzle, setCurrentPuzzle] = useState(null);
-  // const [saveError, setSaveError] = useState(null);
-  // const [saveSuccessMessage, setSaveSuccessMessage] = useState(null);
-  // const [isLoading, setIsLoading] = useState(false);
+  const gliftInstance = useRef(null) // Keep track of the glift instance
 
-  const potentialPuzzle = useSelector(
+  const userPuzzle = useSelector(
     (state) => state.userPuzzles.userPuzzle
   );
 
-  const isBoardInitialized = useRef(false); // Keep track of board initialization
-  const [gliftState, setGliftState] = useState(null);
-
   useEffect(() => {
-    // Fetch the puzzle data when the component mounts, put query parameters inside the thunk?
+    // Fetch the userpuzzle data when the component mounts
     dispatch(fetchUserPuzzleByIdThunk(puzzle_id));
   }, [dispatch, puzzle_id]);
 
+  useEffect(() => {
+    if (userPuzzle) {
+      const puzzle = userPuzzle.Puzzle; // userPuzzle has a nested Puzzle object, which contains the relevant puzzle data
+      const userPuzzleSgf = {
+        sgfString: puzzle.sgf_data,
+        initialPosition: puzzle.move_number - 1,
+        problemConditions: { C: ["CORRECT"] },
+        widgetType: "STANDARD_PROBLEM",
+      };
 
-  // useEffect(() => {
-  //   // Ensures Glift board will only be initialized when there's a valid puzzleData and if it hasn't been initialized already
-  //   // isBoardinitialized starts as false, so it will pass the first time
-  //   if (puzzleData && !isBoardInitialized.current) {
-  //     // Prevent further reintializations of the board
-  //     isBoardInitialized.current = true;
-  //     let checkCorrectHook = new glift.api.HookOptions({
-  //       problemCorrect: onProblemCorrect,
-  //       problemIncorrect: onProblemIncorrect,
-  //     });
+      const instance = glift.create({
+        divId: "gliftContainer",
+        sgf: userPuzzleSgf,
+        display: {
+          drawBoardCoords: true,
+          disableZoomForMobile: true,
+        },
+      });
 
-  //     setGliftState(
-  //       glift.create({
-  //         divId: "gliftContainer",
-  //         sgf: {
-  //           sgfString: puzzleData.sgf_data, // sgf_data column from the database
-  //           initialPosition: puzzleData.move_number - 1, // move_number is the column from the database, need to go back one because the mistake happens on the move_number
-  //           problemConditions: { C: ["CORRECT"] },
-  //           widgetType: "STANDARD_PROBLEM",
-  //         },
-  //         display: {
-  //           drawBoardCoords: true,
-  //           disableZoomForMobile: true,
-  //           // theme: "DEPTH",
-  //         },
-  //         hooks: checkCorrectHook,
-  //       })
-  //     );
+      gliftInstance.current = instance;
 
-  //     // Cleanup function, otherwise we need a hard refresh to show the puzzle (it'll show the previous puzzle we were on)
-  //     return () => {
-  //       if (gliftState) {
-  //         gliftState.destroy();
-  //       }
-  //       glift.api.iconActionDefaults["problem-explanation"].click =
-  //         originalClick;
-  //       glift.api.iconActionDefaults["problem-explanation"].tooltip =
-  //         "Explore the solution";
-  //       isBoardInitialized.current = false; // Reset the flag here otherwise the Go board doesn't show up when we swap to a different puzzle
-  //     };
-  //   }
-  //   // gliftState in the dependency array messes up the rendering as it goes into an infinite loop
-  //   // eslint-disable-next-line
-  // }, [puzzleData, onProblemCorrect, onProblemIncorrect, originalClick]);
+      // Clean up the glift instance
+      return () => {
+        instance.destroy();
+      };
+    }
+  }, [userPuzzle]);
 
   return (
     <>
