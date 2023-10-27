f6aa235590a41cb565ae72e2f53e5ca152b66254
nharwit <nharwit@gmail.com>
2023-10-27 16:58:07 -0600

    started page 3, 4, and 5 for basic rules

---
+++ frontend/src/components/LearnAndPractice/BasicRulesPage3.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BasicRulesPage3 = () => {
+
+  return (
+    <div className="tutorial-puzzle-page">
+      <h2>Basic Rules: Placing Stones & Surrounding Territory 3</h2>
+      <p>Who won the game below?</p>
+    </div>
+  );
+};
+
+export default BasicRulesPage3;
