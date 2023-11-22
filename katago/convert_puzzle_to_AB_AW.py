941d8f22d08d3e375d6401df6a9fbf1b4580fa81
nharwit <nharwit@gmail.com>
2023-11-21 20:18:35 -0500

    removed comments and unnecessary portions of the convert to AB AW script, moved output to tests folder instead

--- katago/convert_puzzle_to_AB_AW.py
+++ katago/convert_puzzle_to_AB_AW.py
@@ -1,5 +1,4 @@
 from sgfmill import sgf, boards
-import re
 
 def to_go_coordinates(row, col):
     # Adjust the column index to skip 'I'
@@ -84,15 +83,12 @@ def convert_sgf_data_to_AB_AW(sgf_data):
     else:
         first_part = original_sgf_data
 
-    # print(first_part)
     second_part = ab_string + aw_string
-
-    # print(first_part+second_part)
 
     last_part_index_b = original_sgf_data.find("(;B")
     last_part_index_w = original_sgf_data.find("(;W")
 
-    # If neither ';B' nor ';W' is found, slice_index will be -1
+    # If neither '(;B' nor '(;W' is found, slice_index will be -1
     slice_index_end = min(last_part_index_b if last_part_index_b != -1 else float('inf'),
                       last_part_index_w if last_part_index_w != -1 else float('inf'))
 
@@ -102,8 +98,3 @@ def convert_sgf_data_to_AB_AW(sgf_data):
         third_part = original_sgf_data
 
     return first_part + second_part + third_part
-
-# Convert the SGF data
-converted_sgf_data = convert_sgf_data_to_AB_AW("(;EV[9th Ing Cup, final 2]DT[2023-08-23]PB[Xie Ke]BR[9p]PW[Shin Jinseo]WR[9p]KM[7.5]RE[W+R]SO[Go4Go.net]CA[UTF-8];B[qd];W[dd];B[pq];W[dp];B[oc];W[qo];B[np];W[nd];B[cc];W[cd];B[dc];W[fc];B[fb];W[gb];B[ec];W[fd];B[ed];W[ee];B[de];W[be];B[df];W[ef];B[eg];W[fe];B[bc];W[dg];B[bd];W[cg];B[ce];W[nc];B[od];W[ne];B[fg];W[di];B[if];W[jd];B[fi];W[dk];B[cq];W[cp];B[dq];W[ep];B[eq];W[fq];B[fr];W[gq];B[gr];W[hq] (;B[qk]C[Incorrect - This was the actual move played in the game!]) (;B[fl]C[CORRECT] ;W[hi]C[CORRECT] ;B[ej]C[CORRECT]) (;B[ii]C[CORRECT] ;W[qe]C[CORRECT] ;B[oe]C[CORRECT] ;W[rd]C[CORRECT]) (;B[ih]C[CORRECT] ;W[qe]C[CORRECT] ;B[oe]C[CORRECT] ;W[rd]C[CORRECT]) (;B[ij]C[CORRECT] ;W[qe]C[CORRECT] ;B[oe]C[CORRECT]) (;B[ji]C[CORRECT] ;W[gj]C[CORRECT] ;B[fj]C[CORRECT]) (;B[ke]C[CORRECT] ;W[kd]C[CORRECT] ;B[fl]C[CORRECT] ;W[qe]C[CORRECT]) (;B[gl]C[CORRECT] ;W[qe]C[CORRECT] ;B[oe]C[CORRECT]) (;B[ej]C[CORRECT] ;W[dj]C[CORRECT] ;B[ii]C[CORRECT] ;W[qe]C[CORRECT]) (;B[hc]C[CORRECT] ;W[hb]C[CORRECT] ;B[ic]C[CORRECT]) (;B[fk]C[CORRECT] ;W[qe]C[CORRECT]))")
-
-print(converted_sgf_data)
---
+++ tests/tests_for_mutating_sgfs/08_convert_puzzle_to_AB_AW_test.py
@@ -0,0 +1,15 @@
+from katago.convert_puzzle_to_AB_AW import to_go_coordinates, convert_to_sgf, convert_sgf_data_to_AB_AW
+
+def test_puzzle_to_AB_AW():
+  ''' This function tests to make sure a puzzle for glift that has a move order,
+  denoted by ;B and ;W is converted to the exact same puzzle, but without move order,
+  this is because glift renders certain ghost stones sometimes, which may be causing some nasty
+  rendering bugs.
+
+  Copy the output into a SGF editor, or render it on localhost:5500
+  with glift by creating a file and referencing it in /glift/sgfUp2Puzzle.html
+  '''
+
+  converted_sgf_data = convert_sgf_data_to_AB_AW("(;EV[9th Ing Cup, final 2]DT[2023-08-23]PB[Xie Ke]BR[9p]PW[Shin Jinseo]WR[9p]KM[7.5]RE[W+R]SO[Go4Go.net]CA[UTF-8];B[qd];W[dd];B[pq];W[dp];B[oc];W[qo];B[np];W[nd];B[cc];W[cd];B[dc];W[fc];B[fb];W[gb];B[ec];W[fd];B[ed];W[ee];B[de];W[be];B[df];W[ef];B[eg];W[fe];B[bc];W[dg];B[bd];W[cg];B[ce];W[nc];B[od];W[ne];B[fg];W[di];B[if];W[jd];B[fi];W[dk];B[cq];W[cp];B[dq];W[ep];B[eq];W[fq];B[fr];W[gq];B[gr];W[hq] (;B[qk]C[Incorrect - This was the actual move played in the game!]) (;B[fl]C[CORRECT] ;W[hi]C[CORRECT] ;B[ej]C[CORRECT]) (;B[ii]C[CORRECT] ;W[qe]C[CORRECT] ;B[oe]C[CORRECT] ;W[rd]C[CORRECT]) (;B[ih]C[CORRECT] ;W[qe]C[CORRECT] ;B[oe]C[CORRECT] ;W[rd]C[CORRECT]) (;B[ij]C[CORRECT] ;W[qe]C[CORRECT] ;B[oe]C[CORRECT]) (;B[ji]C[CORRECT] ;W[gj]C[CORRECT] ;B[fj]C[CORRECT]) (;B[ke]C[CORRECT] ;W[kd]C[CORRECT] ;B[fl]C[CORRECT] ;W[qe]C[CORRECT]) (;B[gl]C[CORRECT] ;W[qe]C[CORRECT] ;B[oe]C[CORRECT]) (;B[ej]C[CORRECT] ;W[dj]C[CORRECT] ;B[ii]C[CORRECT] ;W[qe]C[CORRECT]) (;B[hc]C[CORRECT] ;W[hb]C[CORRECT] ;B[ic]C[CORRECT]) (;B[fk]C[CORRECT] ;W[qe]C[CORRECT]))")
+
+  print(converted_sgf_data)
