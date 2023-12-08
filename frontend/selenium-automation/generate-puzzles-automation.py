f221ba65fa7348fb8d0f1fb0ce08a1e8dff044cb
nharwit <nharwit@gmail.com>
2023-12-08 11:29:47 -0700

    added time.sleep() to have selenium not insta close chrome

--- frontend/selenium-automation/generate-puzzles-automation.py
+++ frontend/selenium-automation/generate-puzzles-automation.py
@@ -1,32 +1,37 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
+
 # Initialize the WebDriver
 driver = webdriver.Chrome()
 
 # Open the webpage with SGFs
 driver.get("https://go-puzzles.com/sgfs")
+time.sleep(30)
 
-# Find all the 'Generate Puzzles' buttons
-generate_buttons = driver.find_elements(By.CLASS_NAME, "generate-puzzle-button-class")
+# Find the login button, and log in
+login_button = driver.find_element(By.CLASS_NAME, "")
+# # Find all the 'Generate Puzzles' buttons
+# generate_buttons = driver.find_elements(By.CLASS_NAME, "create-puzzles-button")
 
-# Maximum timeout duration before moving onto next button click
-timeout_duration = 120
+# # Maximum timeout duration before moving onto next button click
+# timeout_duration = 90
 
-# Loop through each button and click it
-for button in generate_buttons:
-    button.click()
+# # Loop through each button and click it
+# for button in generate_buttons:
+#     button.click()
 
-    try:
-        # Wait for puzzle generation to complete
-        WebDriverWait(driver, timeout_duration).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "success-message-class"))
-        )
-    except TimeoutException:
-        print("Timed out waiting for puzzle generation. Moving to the next one.")
+#     try:
+#         # Wait for puzzle generation to complete
+#         WebDriverWait(driver, timeout_duration).until(
+#             EC.presence_of_element_located((By.CLASS_NAME, "success-notification"))
+#         )
+#     except TimeoutException:
+#         print("Timed out waiting for puzzle generation. Moving to the next one.")
 
-# Close the driver
-driver.quit()
+# # Close the driver
+# driver.quit()
