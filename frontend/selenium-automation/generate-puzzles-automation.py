2540fa7abd7c0c704108869d3839e3dfbda0439d
nharwit <nharwit@gmail.com>
2023-12-10 00:16:42 -0700

    selenium script properly ran, still has 2 bugs, one with ~20 sgfs uploading properly -> error on frontend of webpage was 'Failed to convert SGF data', meaning it's almost certainly an issue with an invalid one line json input conversion, the 2nd bug is selenium can't find the error notification message so it just times out after 30 seconds instead of immediately moving onto the next sgf

--- frontend/selenium-automation/generate-puzzles-automation.py
+++ frontend/selenium-automation/generate-puzzles-automation.py
@@ -5,11 +5,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
+start_time = time.time()
+
 # Initialize the WebDriver
 driver = webdriver.Chrome()
 # Open the webpage
 driver.get("https://go-puzzles.com")
-# Find the login button, and log in
 driver.implicitly_wait(5) # wait up to 5 seconds to find the login button element
 # Find the 2nd li element within the parent h2 element with className specified, this will be the login button
 login_button = driver.find_element(By.XPATH, "//h2[@class='landing-page-signup-login-button']/li[2]")
@@ -30,24 +31,35 @@ login_submit_button.click()
 driver.get("https://go-puzzles.com/sgfs")
 # wait 5 seconds to let the page load ~100 SGFs just in case not all of them load right away,
 # we are not using implicitly wait because a fraction of the sgfs may load initially, but not all of them, causing complications with implicitly wait
-time.sleep(5)
+time.sleep(2)
 # # Find all the 'Generate Puzzles' buttons
-# generate_buttons = driver.find_elements(By.CLASS_NAME, "create-puzzles-button")
+generate_buttons = driver.find_elements(By.CLASS_NAME, "create-puzzles-button")
 
-# # Maximum timeout duration before moving onto next button click
-# timeout_duration = 90
+# print(len(generate_buttons)) # properly outputs 120 -> that's the number of generate puzzle buttons that are available because there are 120 sgfs
 
-# # Loop through each button and click it
-# for button in generate_buttons:
-#     button.click()
+# Maximum timeout duration before moving onto next button click
+timeout_duration = 30
 
-#     try:
-#         # Wait for puzzle generation to complete
-#         WebDriverWait(driver, timeout_duration).until(
-#             EC.presence_of_element_located((By.CLASS_NAME, "success-notification"))
-#         )
-#     except TimeoutException:
-#         print("Timed out waiting for puzzle generation. Moving to the next one.")
+total_buttons = len(driver.find_elements(By.CLASS_NAME, "create-puzzles-button"))
+for i in range(total_buttons):
+    # Refetch the button list and get the current button (to avoid stale buttons causing Selenium to miss buttons)
+    button = driver.find_elements(By.CLASS_NAME, "create-puzzles-button")[i]
+    time.sleep(2)
+    button.click()
 
-# # Close the driver
-# driver.quit()
+    try:
+        # Wait for puzzle generation to complete, showing either success message, or error message, if neither show up, it means the process is hanging on Google Clou
+        WebDriverWait(driver, timeout_duration).until(
+            lambda d: EC.presence_of_element_located((By.CLASS_NAME, "success-notification"))(d) or
+            EC.presence_of_element_located((By.CLASS_NAME, "error-notification"))(d) # Selenium isn't finding this element for some reason!
+        )
+        time.sleep(2) # Either this time.sleep(2) or the one below the line here: button = driver.find_elements(By.CLASS_NAME, "create-puzzles-button")[i] fixed the bug where selenium would skip like 50 sgfs at a time and only click 9 buttons before getting through all 120 sgfs
+    except TimeoutException:
+        print("Timed out waiting for puzzle generation. Moving to the next one.")
+
+# Close the driver
+driver.quit()
+
+end_time = time.time()
+
+print("elapsed time for 120 sgfs", end_time - start_time) # elapsed time for 120 sgfs 3194.21071767807
